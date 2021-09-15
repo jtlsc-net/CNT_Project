1b886781492244419651f9c8a5e0844e85c5f182
josh <jtlsc@gmail.com>
2021-09-15 11:02:52 -0400

    added first part of handshake ~jlsc

--- Client.java
+++ Client.java
@@ -3,6 +3,7 @@ import java.io.*;
 import java.nio.*;
 import java.nio.channels.*;
 import java.util.*;
+import java.nio.charset.StandardCharsets; // for UTF-8
 
 public class Client {
 	Socket requestSocket;           //socket connect to the server
@@ -13,6 +14,9 @@ public class Client {
     String type;                   //type of msg
     int byte_length;               //number of bytes to store string
     String byte_length_string;     //string for no. of bytes
+	byte[] handshake_message = new byte[32];	   //array for handshake
+	byte[] speed;
+	byte[] ran;
 
     public void Client() {}
     
@@ -29,6 +33,28 @@ public class Client {
 			
 			//get Input from standard input
 			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+			//handshake_message = "P2PFILESHARINGPROJ".getBytes() + {0,0,0,0,0,0,0,0,0,0} + {1,1,1,1};
+			speed = "P2PFILESHARINGPROJ".getBytes();
+			int P2Pbytelength = "P2PFILESHARINGPROJ".getBytes().length;
+			System.out.println(P2Pbytelength + " <- this is the length");
+			for(int z = 0; z < 18; z++)
+			{
+				handshake_message[z] = speed[z];
+			}
+			for(int j = P2Pbytelength; j < 28; j++)
+			{
+				handshake_message[j] = 0;
+			}
+			for(int k = 28; k < 32; k++)
+			{
+				handshake_message[k] = 1;
+			}
+			String q = new String(handshake_message, StandardCharsets.UTF_8);
+			sendMessage(q);
+			MESSAGE = (String)in.readObject();
+			//show the message to the user
+			System.out.println("Receive message: " + MESSAGE);
+			
 			while(true)
 			{
 				System.out.print("Hello, please input a sentence: ");
